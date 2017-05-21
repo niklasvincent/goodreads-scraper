b4b5a2e80a6c14800e2d05f7eee5643a3eb0b2c0
Niklas Lindblad <niklas@lindblad.info>
2017-05-21 19:25:37 +0100

    Fix page counter

--- generate-html.py
+++ generate-html.py
@@ -1,4 +1,5 @@
 import base64
+import datetime
 import os
 from collections import defaultdict, namedtuple
 
@@ -15,11 +16,12 @@ BookWithCover = namedtuple(
 )
 
 
-def render_from_template(template_filename, books_with_cover_by_year, pages_per_day):
+def render_from_template(template_filename, books_with_cover_by_year, pages_per_year):
     path, filename = os.path.split(template_filename)
+    now = datetime.datetime.now()
     return jinja2.Environment(
         loader=jinja2.FileSystemLoader(path or './')
-    ).get_template(filename).render(books_by_year=books_with_cover_by_year, pages_per_day=pages_per_day)
+    ).get_template(filename).render(books_by_year=books_with_cover_by_year, pages_per_year=pages_per_year, current_year=now.year)
 
 
 def get_inline_image(filename):
@@ -45,14 +47,14 @@ def main(input_filename, cover_directory, output_directory):
         pages_by_year[year] += book_with_cover.num_pages
         books_with_cover_by_year[year].append(book_with_cover._asdict())
 
-    pages_per_day = {year: round(pages_by_year[year] / 365.25, 1) for year in pages_by_year.keys() if year != 'Earlier'}
+    pages_per_year = {year: pages_by_year[year] for year in pages_by_year.keys()}
 
     if not os.path.exists(output_directory):
         os.mkdir(output_directory)
 
     output_filename = os.path.join(output_directory, 'reading.html')
     with open(output_filename, 'w') as f:
-        output = render_from_template('templates/reading.html.j2', books_with_cover_by_year, pages_per_day)
+        output = render_from_template('templates/reading.html.j2', books_with_cover_by_year, pages_per_year)
         f.write(output)
 
 
